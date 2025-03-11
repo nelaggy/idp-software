c57877a38c4e077d00d856c90242dd699508ce13
Minsuk Lee <minsuklee02@gmail.com>
2025-03-11 11:10:52 +0000

    updated offroad_controller

--- controllers/offroad_controller.py
+++ controllers/offroad_controller.py
@@ -12,7 +12,8 @@ move backwards
 turn around
 '''
 
-class OffRoadController:
+#not using this anymore
+class OffRoadControllerPrev:
     def __init__(self, line_sensors, wheels, on_complete, sda_pin = 16, scl_pin = 17) -> None:
         self.line_sensors = line_sensors
         self.wheels = wheels
@@ -51,6 +52,9 @@ class OffRoadController:
         self.rspeed = 0
         self.target_lspeed = 50
         self.target_rspeed = 50
+    
+    def on_block(self):
+        pass
 
         
     def on_change(self,values):
@@ -145,10 +149,11 @@ class OffRoadController:
 
         
         
-class OffRoadControllerTest:
-    def __init__(self, line_sensors, wheels) -> None:
+class OffRoadController:
+    def __init__(self, line_sensors, wheels, on_complete) -> None:
         self.line_sensors = line_sensors
         self.wheels = wheels
+        self.on_complete = on_complete
 
         # PID constants (Tuning required)
         self.Kp = 5  # Proportional gain
@@ -159,7 +164,6 @@ class OffRoadControllerTest:
         self.D = 0
         self.I = 0
         self.last_error = 0
-        self.pickup = False
         self.integral = 0
 
         self.lspeed = 0
@@ -167,87 +171,97 @@ class OffRoadControllerTest:
         self.target_lspeed = 50
         self.target_rspeed = 50
 
+        # Servo
+        self.servo = Servo()
+
+        # flags
+        self.turn_180_flag = False
+        self.reverse_flag = False
+        self.pickup_flag = False
+
+        # for turn 180
+        self.prev_values = b'\1\1\1\1'
+        self.left_sensor_detected = False
+        self.middle_l_sensor_detected = False
+        self.middle_r_sensor_detected = False
     
     def on_change(self, values: bytearray) -> None:
         '''Handles sensor input updates and applies PID control'''
 
-        if not self.pickup: # follow line if not self.pickup activated
-            error = 3*values[0] + values[1] - values[2] - 3*values[3]
-            if values == b'\x00\x00\x00\x00': # detects all black
-                self.lost()
-                return
-            self.I += error
-            self.D = error - self.last_error
-            self.last_error = error
-            pid = self.Kp * error + self.Ki * self.I + self.Kd * self.D
+        if not self.pickup_flag: # follow line if not self.pickup_flag activated
+            if not self.reverse_flag: # moving forward
+                error = 3*values[0] + values[1] - values[2] - 3*values[3]
+                if values == b'\x00\x00\x00\x00': # detects all black
+                    self.lost()
+                    return
+                self.I += error
+                self.D = error - self.last_error
+                self.last_error = error
+                pid = self.Kp * error + self.Ki * self.I + self.Kd * self.D
 
-            # Motor speed adjustments
-            self.lspeed = max(0, min(55, self.target_lspeed - pid))
-            self.rspeed = max(0, min(55, self.target_rspeed + pid))
+                # Motor speed adjustments
+                self.lspeed = max(0, min(55, self.target_lspeed - pid))
+                self.rspeed = max(0, min(55, self.target_rspeed + pid))
 
-            self.wheels.wheel_speed(int(self.lspeed), int(self.rspeed))
+                self.wheels.wheel_speed(int(self.lspeed), int(self.rspeed))
+            
+            else: # reverse
+                error = 3*values[0] + values[1] - values[2] - 3*values[3]
+                if values == b'\x00\x00\x00\x00': # detects all black
+                    self.lost()
+                    return
+                self.I += error
+                self.D = error - self.last_error
+                self.last_error = error
+                pid = self.Kp * error + self.Ki * self.I + self.Kd * self.D
+
+                # Motor speed adjustments for backward movement
+                self.lspeed = max(-55, min(0, -self.target_lspeed*3/5 - pid))
+                self.rspeed = max(-55, min(0, -self.target_rspeed*3/5 + pid))
+
+                self.wheels.wheel_speed(int(self.lspeed), int(self.rspeed))
+
+                if values == b'\1\1\1\1' and not self.turn_180_flag:
+                    self.turn_180_flag = True
+                    schedule(time.sleep,2) # move backwards for further two seconds
+                    schedule(self.line_sensors.set_callback,self.turn_180)
+        
         
         else: # pickup box if self.pickup activated
             self.wheels.stop()
             schedule(self.pickup_box, None)
     
+    def on_block(self):
+        self.wheels.stop()
+        self.pickup_flag = True
+        schedule(self.pickup_box,None)
+    
 
     def lost(self) -> None:
         print('lost')
-        # self.wheels.wheel_speed(0,50) # slow spin to search, change
         self.wheels.stop()
     
     def pickup_box(self) -> None:
-        self.move_backwards()
-        self.turn_180()
-        self.wheels.stop()
-        print('turn complete!')
-    
-    def move_backwards(self) -> None:
-        # Move backwards
-        while True:
-            values = self.line_sensors.read()  # Read sensor values
-            if values == b'\1\1\1\1':  # Stop condition when detecting 1111
-                self.wheels.wheel_speed(-30,-30)
-                time.sleep(2) # move backwards for 2 second after the junction
-                self.wheels.stop()
-                break
-            
-            error = 3*values[0] + values[1] - values[2] - 3*values[3]
-            if values == b'\x00\x00\x00\x00': # detects all black
-                self.lost()
-                return
-            self.I += error
-            self.D = error - self.last_error
-            self.last_error = error
-            pid = self.Kp * error + self.Ki * self.I + self.Kd * self.D
+        #self.servo.lift()
+        self.pickup_flag = False
+        self.reverse_flag = True
 
-            # Motor speed adjustments for backward movement
-            self.lspeed = max(-55, min(0, -self.target_lspeed*3/5 - pid))
-            self.rspeed = max(-55, min(0, -self.target_rspeed*3/5 + pid))
+    def turn_180(self, values: bytearray) -> None:
 
-            self.wheels.wheel_speed(int(self.lspeed), int(self.rspeed))
-            
+        if not (self.left_sensor_detected and self.middle_l_sensor_detected and self.middle_r_sensor_detected):
 
-    def turn_180(self) -> None:
-        left_sensor_detected = False
-        middle_l_sensor_detected = False
-        middle_r_sensor_detected = False
-
-        prev_values = list(self.line_sensors.read()) # initial sensor readings
-
-        while not (left_sensor_detected and middle_l_sensor_detected and middle_r_sensor_detected):
-            values = list(self.line_sensors.read()) # read current sensor values
-
-            if not left_sensor_detected and prev_values[0] == 0 and values[0] == 1:
-                left_sensor_detected = True
-            if not middle_l_sensor_detected and prev_values[1] == 0 and values[1] == 1:
-                middle_l_sensor_detected = True
-            if not middle_r_sensor_detected and prev_values[2] == 0 and values[2] == 1:
-                middle_r_sensor_detected = True
+            if not self.left_sensor_detected and self.prev_values[0] == 0 and values[0] == 1:
+                self.left_sensor_detected = True
+            if not self.middle_l_sensor_detected and self.prev_values[1] == 0 and values[1] == 1:
+                self.middle_l_sensor_detected = True
+            if not self.middle_r_sensor_detected and self.prev_values[2] == 0 and values[2] == 1:
+                self.middle_r_sensor_detected = True
 
             self.wheels.wheel_speed(-15,15) # keep turning left
-            prev_values = values # update previous sensor values
+            self.prev_values = values # update previous sensor values
+
+        else:
+            self.on_complete
             
     def activate(self) -> None:
         self.line_sensors.set_callback(self.on_change)
--- offroad_main.py
+++ offroad_main.py
@@ -2,12 +2,12 @@ from hardware.motor import Motors
 from hardware.line_sensor import LineSensors
 from navigation.navigator import Navigator
 from controllers.offroad_controller import OffRoadControllerTest
+from dummy.servo import servo_test
 
 line_sensors = LineSensors(None)
 wheels = Motors()
 controller = OffRoadControllerTest(line_sensors, wheels)
 
-# go to A
 controller.activate()
 while True:
     pass
