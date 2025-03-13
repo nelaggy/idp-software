705f8beef8c41b5e065824211c4ca0d284d6afa3
Minsuk Lee <minsuklee02@gmail.com>
2025-03-13 16:28:32 +0000

    color_sensor update

--- hardware/color_sensor.py
+++ hardware/color_sensor.py
@@ -10,15 +10,18 @@ class ColourSensor:
         self.i2c_bus = I2C(0, sda=Pin(sda_pin), scl=Pin(scl_pin))
         self.tcs = TCS34725(self.i2c_bus)
     
-    def detect(self):
+    def detect(self): # True if green or blue, false if red or yellow
         cct_sum = 0
+        B_sum = 0
         n = 0
         start_time = time.time()
 
         while time.time() - start_time < 3: # Run for 3 seconds
             cct_sum += self.tcs.read(raw=False)[0] # Get cct value
+            B_sum += self.tcs.read(raw=True)[2] # Get blue value
             n += 1
             time.sleep(0.1)
         
         cct_average = cct_sum/n
-        return cct_average < 8000
+        B_average = B_sum/n
+        return cct_average > 10000 or B_average > 10
